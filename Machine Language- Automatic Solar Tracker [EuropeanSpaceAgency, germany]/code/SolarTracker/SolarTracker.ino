1f3376040c44899a62ee3674ed7c0350bab477be
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-08-31 18:23:41 +0530

    Create SolarTracker.ino

--- Machine Language- Automatic Solar Tracker [EuropeanSpaceAgency, germany]/code/SolarTracker/SolarTracker.ino
+++ Machine Language- Automatic Solar Tracker [EuropeanSpaceAgency, germany]/code/SolarTracker/SolarTracker.ino
@@ -5,7 +5,6 @@
   surface area exposure to sunlight with automatic nightly orientation reset. This will 
   optimize solar power generation with 40%-60% improvement in efficiency over fixed PV 
   panel installations.
-
   The circuit:
    
 
